f2857d39b32a23782b5e61e965bcf28d36cc59e4
MoatazAllam <mezo.allam2015@hotmail.com>
2021-05-20 15:35:04 +0200

    Add project files.

---
+++ Vidly_MVC_Training/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_MVC_Training.Startup))]
+namespace Vidly_MVC_Training
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
